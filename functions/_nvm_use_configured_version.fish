0d72f897839065a50846b57378c323639f40b587
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 13:58:22 -0700

    Polish.

--- functions/_nvm_use_configured_version.fish
+++ functions/_nvm_use_configured_version.fish
@@ -2,38 +2,25 @@
 # If found, compare the configured NodeJS version with the active version
 # Switch to the configured version with if they differ
 function _nvm_use_configured_version
-    echo using configured version
     # Check in CWD
     if test -e .nvmrc
-        echo cwd found
-        cat .nvmrc
         set -f nvmrc_path .nvmrc
-    end
-
-    echo checking git root
     # Check at git root
-    if git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    else if git rev-parse --is-inside-work-tree >/dev/null 2>&1
         and set -f git_root (git rev-parse --show-toplevel)
         and test -e $git_root/.nvmrc
 
-        echo $git_root
-        echo git root found
         set -f nvmrc_path $git_root/.nvmrc
     end
 
-    echo path $nvmrc_path
-
     # Do nothing if not found
     if not set -q nvmrc_path
-        echo not found
         return
     end
 
     set -f nvmrc_version (cat $nvmrc_path)
-    echo attempting switch to $nvmrc_version
 
     if test "$nvmrc_version" = "$nvm_current_version"
-        echo same version
         return
     end
 
