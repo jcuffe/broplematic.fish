9b19676670572dda8e308418a042d068c837f43e
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 14:06:19 -0700

    Polish round 2.

--- conf.d/__pwd_onchange_hooks.fish
+++ conf.d/__pwd_onchange_hooks.fish
@@ -1,5 +1,5 @@
 # Logic to run when the working directory changes
 function pwd_onchange_hooks -v PWD
-    echo onchange baby
+    # Manage NodeJS version with nvm and .nvmrc files
     _nvm_use_configured_version
 end
--- functions/_nvm_use_configured_version.fish
+++ functions/_nvm_use_configured_version.fish
@@ -1,6 +1,6 @@
 # Look for an .nvmrc file in the current working directory, or the git root
-# If found, compare the configured NodeJS version with the active version
-# Switch to the configured version with if they differ
+# If found, switch to the configured version if it differs from the active version
+# Otherwise, switch to the default version if it is configured
 function _nvm_use_configured_version
     # Check in CWD
     if test -e .nvmrc
@@ -13,16 +13,13 @@ function _nvm_use_configured_version
         set -f nvmrc_path $git_root/.nvmrc
     end
 
-    # Do nothing if not found
+    # If not found, ensure we're using the system default
     if not set -q nvmrc_path
+        set -q nvm_default_version && nvm use $nvm_default_version --silent
         return
     end
 
-    set -f nvmrc_version (cat $nvmrc_path)
-
-    if test "$nvmrc_version" = "$nvm_current_version"
-        return
-    end
-
-    nvm use $nvmrc_version
+    # Switch only if our active version differs from expected
+    set -f expected_version (cat $nvmrc_path)
+    test "$expected_version" != "$nvm_current_version" && nvm use $expected_version
 end
