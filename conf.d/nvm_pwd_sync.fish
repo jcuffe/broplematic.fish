6e57ac4799deb8aee320c231f21cde10fc297bdc
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 12:19:12 -0700

    Corrects syntax.

--- conf.d/nvm_pwd_sync.fish
+++ conf.d/nvm_pwd_sync.fish
@@ -1,11 +1,15 @@
 function sync_nvm_with_pwd -v PWD
     # Check for .nvmrc
     # TODO: use git to check the project root as well as the current directory
-    if ! test -e .nvmrc return
+    if ! test -e .nvmrc
+        return
+    end
 
     # Check for nvm function
     # TODO: Ensure this logic is loaded after nvm
-    if ! type -q nvm return
+    if ! type -q nvm
+        return
+    end
 
     # Load the configured Node version for the current project
     nvm use
