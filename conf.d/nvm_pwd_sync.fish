375fae6c252c021d4a9414e67deb2cc7821ce0ae
Jared Cuffe <jcuffe@gmail.com>
2022-07-10 12:12:29 -0700

    Refactors automatic node load.

---
+++ conf.d/nvm_pwd_sync.fish
@@ -0,0 +1,12 @@
+function sync_nvm_with_pwd -v PWD
+    # Check for .nvmrc
+    # TODO: use git to check the project root as well as the current directory
+    if ! test -e .nvmrc return
+
+    # Check for nvm function
+    # TODO: Ensure this logic is loaded after nvm
+    if ! type -q nvm return
+
+    # Load the configured Node version for the current project
+    nvm use
+end
